d3f68895a5445d97a23ad7d4fb1c30c4453e97b5
karinaramos <kramosq@gmail.com>
2024-05-19 16:06:15 -0500

    added sidebar shopping cart

--- main.js
+++ main.js
@@ -2,14 +2,44 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const iconCart = document.querySelector('.navbar-shopping-cart');
+const asideShoppingCart = document.querySelector('#shopping-cart');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 iconMenu.addEventListener('click', toggleMobileMenu);
+iconCart.addEventListener('click', toggleCartAside);
 
 function toggleDesktopMenu() {
+  const isShoppingCartClosed = asideShoppingCart.classList.contains('inactive');
+
+  if(!isShoppingCartClosed) {
+    asideShoppingCart.classList.add('inactive');
+  }
+
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
-  mobileMenu.classList.toggle('activo');
+  const isShoppingCartClosed = asideShoppingCart.classList.contains('inactive');
+
+  if(!isShoppingCartClosed) {
+    asideShoppingCart.classList.add('inactive');
+  }
+
+  mobileMenu.classList.toggle('active');
+}
+
+function toggleCartAside() {
+  const isMobileMenuOpen = mobileMenu.classList.contains('active');
+  const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+  if(!isDesktopMenuClosed) {
+    desktopMenu.classList.add('inactive');
+  }
+  
+  if(isMobileMenuOpen) {
+    mobileMenu.classList.remove('active');
+  }
+
+  asideShoppingCart.classList.toggle('inactive');
 }
